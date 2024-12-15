07353bef6546db669cb7afe9c40ff714d15e4ac7
anandcilans <anand.cilans@gmail.com>
2024-12-15 17:08:56 +0530

    updated

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
 import base64
 
+# system.sh
+!bash system.sh
+
 
 OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
 
---
+++ system.sh
@@ -0,0 +1,3 @@
+# system.sh
+sudo apt-get update
+sudo apt-get install sqlite3
